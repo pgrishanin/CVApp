59c67461860bc20c4c7d4a2aa8e6ac56ea146cb0
Pavel Grishanin <pgrishanin@gmail.com>
2020-10-23 22:28:50 +0300

    Initial Commit

---
+++ CVApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CVApp
+//
+//  Created by pavel on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
